83d15a2f2d21708581a1577f7d8dd4251cf0d36e
Stephen Nimmo <stephennimmo@gmail.com>
2020-11-30 10:24:11 -0600

    deleted everything

--- kafka-consumer/src/main/java/com/redhat/energy/kafka/KafkaConsumerMain.java
+++
@@ -1,8 +0,0 @@
-@QuarkusMain
-public class KafkaConsumerMain implements QuarkusApplication {
-    @Override
-    public int run(String... args) throws Exception {   
-      System.out.println("Hello World");
-      return 10;
-   }
-  }
